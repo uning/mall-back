7b6dc7c120f0eeeefc4d66ebc72be5d62724e298
JimmyChou <zxm@playcrab.com>
2010-08-09 23:28:07 +0800

    ok

--- web/public/webreject/controller/Tool.php
+++ web/public/webreject/controller/Tool.php
@@ -36,32 +36,21 @@ class Tool
 		$ret['bgoods'] = $tu->get( TT::GOODS_GROUP );
 		$aid = $tu->getoid( 'advert',TT::OTHER_GROUP );
 		$ret['aid'] = $aid;
-		$ret['badverts'] = $tu->getbyid( $aid );	
+		$ret['badverts'] = $tu->getbyid( $aid );
+		$ret['bcars'] = $tu->get( TT::CAR_GROUP );	
 		foreach( $ret['bitems'] as $item ){
 			$itemids[] = $item['id'];
 		}		
 		foreach( $ret['bgoods'] as $goods ){
 			$goodsids[] = $goods['id'];
 		}
+		foreach( $ret['bcars'] as $car ){
+			$carids[] = $car['id'];
+		}		
 		$tu->remove( $aid );
 		$tu->remove( $itemids );
 		$tu->remove( $goodsids );
-		$ret['aua'] = $tu->getAll();
-		$ret['aitems'] = $tu->get( TT::ITEM_GROUP  );
-		$ret['agoods'] = $tu->get( TT::GOODS_GROUP );
-		$ret['aadverts'] = $tu->getbyid( $aid );	
-		return $ret;
-	}
-	
-	public function all ( $params )
-	{
-		$uid = $params['u'];
-		$tu = new TTUser($uid);
-		/*
-		$ret['bitems'] = $tu->get( TT::ITEM_GROUP );
-		$ret['bgoods'] = $tu->get( TT::GOODS_GROUP );
-		$ret['bcars'] = $tu->get( TT::CAR_GROUP );
-		*/
+		$tu->remove( $carids );
 		$ret['bua'] = $tu->getf( array( TT::MONEY_STAT,TT::GEM_STAT,TT::EXP_STAT,TT::COMPUTE_PONIT,TT::SHOP_NUM,TT::POPU,TT::GARAGE_STAT,TT::CAPACITY_STAT,TT::TASK_STAT,'lastawardtime','continued') );
 //		$data = array( TT::MONEY_STAT,TT::GEM_STAT,TT::EXP_STAT,TT::COMPUTE_PONIT,TT::SHOP_NUM,TT::POPU,TT::GARAGE_STAT,TT::CAPACITY_STAT,TT::TASK_STAT,'lastawardtime','continued');
 //		$tu->mputf( $data );
@@ -76,20 +65,12 @@ class Tool
         $tu->numch( 'continued',0 - $ret['bua']['continued'] );
         $data = array( TT::CAPACITY_STAT=>"3,2",TT::TASK_STAT=>'1' );
         $tu->mputf( $data );
-		$ret['aua'] = $tu->getf( array( TT::MONEY_STAT,TT::GEM_STAT,TT::EXP_STAT,TT::COMPUTE_PONIT,TT::SHOP_NUM,TT::POPU,TT::GARAGE_STAT,TT::CAPACITY_STAT,TT::TASK_STAT,'lastawardtime','continued') );
-		$aid = $tu->getoid( 'advert',TT::OTHER_GROUP );
-		$ret['aid'] = $aid;
-		$ret['badverts'] = $tu->getbyid( $aid );	
-		foreach( $ret['bitems'] as $item ){
-			$itemids[] = $item['id'];
-		}		
-		foreach( $ret['bgoods'] as $goods ){
-			$goodsids[] = $goods['id'];
-		}
-		foreach( $ret['bcars'] as $car ){
-			$carids[] = $car['id'];
-		}
-//		$ret['ball'] = $tu->getAll();
+		$ret['aua'] = $tu->getf( array( TT::MONEY_STAT,TT::GEM_STAT,TT::EXP_STAT,TT::COMPUTE_PONIT,TT::SHOP_NUM,TT::POPU,TT::GARAGE_STAT,TT::CAPACITY_STAT,TT::TASK_STAT,'lastawardtime','continued') );		
+		$ret['aua'] = $tu->getAll();
+		$ret['aitems'] = $tu->get( TT::ITEM_GROUP  );
+		$ret['agoods'] = $tu->get( TT::GOODS_GROUP );
+		$ret['acras'] = $tu->get( TT::CAR_GROUP );
+		$ret['aadverts'] = $tu->getbyid( $aid );	
 		return $ret;
 	}
 }

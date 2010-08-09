84f9b7f2cf1c8bde941d5008ff0a04393360859e
JimmyChou <zxm@playcrab.com>
2010-08-09 23:14:09 +0800

    ok

--- web/public/webreject/controller/Tool.php
+++ web/public/webreject/controller/Tool.php
@@ -57,10 +57,26 @@ class Tool
 	{
 		$uid = $params['u'];
 		$tu = new TTUser($uid);
+		/*
 		$ret['bitems'] = $tu->get( TT::ITEM_GROUP );
 		$ret['bgoods'] = $tu->get( TT::GOODS_GROUP );
 		$ret['bcars'] = $tu->get( TT::CAR_GROUP );
+		*/
 		$ret['bua'] = $tu->getf( array( TT::MONEY_STAT,TT::GEM_STAT,TT::EXP_STAT,TT::COMPUTE_PONIT,TT::SHOP_NUM,TT::POPU,TT::GARAGE_STAT,TT::CAPACITY_STAT,TT::TASK_STAT,'lastawardtime','continued') );
+//		$data = array( TT::MONEY_STAT,TT::GEM_STAT,TT::EXP_STAT,TT::COMPUTE_PONIT,TT::SHOP_NUM,TT::POPU,TT::GARAGE_STAT,TT::CAPACITY_STAT,TT::TASK_STAT,'lastawardtime','continued');
+//		$tu->mputf( $data );
+        $tu->numch( TT::MONEY_STAT,10000 - $ret['bua']['money'] );
+        $tu->numch( TT::GEM_STAT,1 - $ret['bua']['gem'] );
+        $tu->numch( TT::EXP_STAT,0 - $ret['bua']['money'] );
+        $tu->numch( TT::COMPUTE_PONIT,0 - $ret['bua']['compute'] );
+        $tu->numch( TT::SHOP_NUM,2 - $ret['bua'][TT::SHOP_NUM] );
+        $tu->numch( TT::POPU,30 - $ret['bua'][TT::POPU] );
+        $tu->numch( TT::GARAGE_STAT,1 - $ret['bua'][TT::GARAGE_STAT] );
+        $tu->numch( 'lastawardtime',10000 - $ret['bua']['lastawardtime'] );
+        $tu->numch( 'continued',10000 - $ret['bua']['continued'] );
+        $data = array( TT::CAPACITY_STAT=>"3,2",TT::TASK_STAT=>'1' );
+        $tu->mpuf( $data );
+		$ret['aua'] = $tu->getf( array( TT::MONEY_STAT,TT::GEM_STAT,TT::EXP_STAT,TT::COMPUTE_PONIT,TT::SHOP_NUM,TT::POPU,TT::GARAGE_STAT,TT::CAPACITY_STAT,TT::TASK_STAT,'lastawardtime','continued') );
 		$aid = $tu->getoid( 'advert',TT::OTHER_GROUP );
 		$ret['aid'] = $aid;
 		$ret['badverts'] = $tu->getbyid( $aid );	

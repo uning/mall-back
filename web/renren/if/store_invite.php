04ee79f920a4f39af0b89fbf94f124df18cd7e49
admin <hi@playcrab.com>
2010-08-10 13:55:20 +0800

    ok

--- web/renren/if/store_invite.php
+++ web/renren/if/store_invite.php
@@ -10,11 +10,11 @@ if($pid &&$ids && $linkid){
 	$_REQUEST['id']=$linkid;
 	$_REQUEST['invalid'] = false;
 	$tw->puto($_REQUEST);
-	file_put_contents('store_invite.txt',print_r($_REQUEST,true));
+	file_put_contents('store_invite1.txt',print_r($_REQUEST,true));
 }
-file_put_contents('store_invite.txt',print_r($_REQUEST,true));
+file_put_contents('store_invite2.txt',print_r($_REQUEST,true));
 header('Location: '.RenrenConfig::$canvas_url.'?f=invite');
-//echo "<pre>\n";
-//print_r($_REQUEST);
+echo "<pre>\n";
+print_r($_REQUEST);
 	
 
--- web/renren/test.php
+++ web/renren/test.php
@@ -1,5 +1,5 @@
 <?php
-    $myloc=dirname(__FILE__);
+/*	$myloc=dirname(__FILE__);
     require_once  $myloc.'/bg/base.php'; 
 	require_once LIB_ROOT.'ModelFactory.php';
 	require_once LIB_ROOT.'AutoIncIdGenerator.php';
@@ -8,4 +8,11 @@
 	$user_id = AutoIncIdGenerator::genid($platform_id);
 	$p_user = AutoIncIdGenerator::genid($pid);
 	echo "user_id = $user_id\n";
-	echo "p_user = $p_user\n";
+	echo "p_user = $p_user\n";*/
+require_once('config.php');
+$linkid =':4c60c7314f3ef';
+$tw = TT::TTWeb();
+	echo 'i.m here<br>';	
+	$irec = $tw->getbyid($linkid);
+	print_r($irec);
+?>

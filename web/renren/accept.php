5123f60a8cb5b48c5f5c7f3db05d9b425d97e115
lyb <big@playcrab.com>
2010-08-10 17:57:41 +0800

    hjd

--- web/renren/accept.php
+++ web/renren/accept.php
@@ -28,6 +28,12 @@ window.location="http://apps.renren.com/livemall";
 }
 //-->
 </script>
+<<style type="text/css">
+<!--
+.giftformsubmit { border-style: solid; border-color: #d9dfea #0e1f5b #0e1f5b #d9dfea; border-width: 1px; margin: 1px 5px; padding: 3px 10px; background-color: #3b5998; color: white; font-size: 12px; font-weight: bold; text-decoration: none; height: 25px; }
+.giftButtonFloat { float:center; margin-top: 1px; margin-bottom: 12px; }
+-->
+</style>
 <xn:if-is-app-user>
 <?php
 	$fsess = TTGenid::getbypid($fromuser);	
@@ -62,8 +68,7 @@ window.location="http://apps.renren.com/livemall";
  .giftFrom_name { padding-top: 20px; }
  .giftFrom_name a:link,  .giftFrom_name a:visited { color: #3b5998; font-weight: bold; text-decoration: underline; }
  .giftFrom_name a:hover,  .giftFrom_name a:active { color: #559890; text-decoration: none; }
-.giftformsubmit { border-style: solid; border-color: #d9dfea #0e1f5b #0e1f5b #d9dfea; border-width: 1px; margin: 1px 5px; padding: 3px 10px; background-color: #3b5998; color: white; font-size: 12px; font-weight: bold; text-decoration: none; height: 25px; }
-.giftButtonFloat { float:center; margin-top: 1px; margin-bottom: 12px; }
+
 	</style>
 		<div class="padding_content center">
 		<div class="main_giftConfirm_cont">

4215f5ede401cf291c6bccd0455f418e15d29c56
hotel@local.centos <hotel@local.centos>
2010-08-11 04:36:21 -0400

    feedback

--- web/public/config.php
+++ web/public/config.php
@@ -9,7 +9,7 @@ define('CURRENT_PLATFORM','test'); //vk,renren,manyou,facebook //(const is for s
 # tools from lib
 require_once LIB_ROOT.'CrabTools.php';
 define('ZEND_ROOT',WORK_ROOT.'backend_common'.DS);
-set_include_path(ZEND_ROOT. PATH_SEPARATOR.PATH_SEPARATOR.get_include_path());
+set_include_path(ZEND_ROOT.PATH_SEPARATOR.get_include_path());
 require_once LIB_ROOT.'Logger.php';
 require_once CONTROLLER_ROOT.'BaseController.php';
 /*

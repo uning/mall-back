26d0a204a0a0281fba112d91c353df85b60f2fe4
unknown <Kevin@.(none)>
2010-08-18 16:12:59 +0800

    'x'

--- web/renren/pay.php
+++ web/renren/pay.php
@@ -94,6 +94,21 @@
 		ajax.post('<?php echo RenrenConfig::$canvas_url."/pay_new_order.php";?>',params, true);
 	}
   </script>
+<div id="header">
+    <div id="navga">
+    <div class="logo"><a href="http://apps.renren.com/livemall/" target="_top" title="开始游戏!">logo</a></div>
+   <div id="tabs">
+    <ul class="clearfix tcenter">       
+        <li class="game" id="flashTab" ><a class="active" href="#switchToFlash" >游戏</a></li>
+        <li class="freegift"><a href="../pop/gift.php" id="freeGift" >免费礼物</a></li>
+        <li class="invite" ><a href="../pop/invite/invite.php" id="invite" >邀请好友</a></li>
+        <li class="faq"><a  href="../static/help/teach.html" class="fullpage" target="_blank">常见问题</a></li>
+        <li class="problem"><a  href="javascript:alert('暂未开放');"  id="problem">问题反馈</a></li>
+		<li class="payment" ><a  href="http://apps.renren.com/livemall/pay.php"   id ="pay">充值</a></li>
+	</ul>
+	</div>
+    </div>
+</div>
 
 <div id='is_install'></div>		
 <div class="poker_game">

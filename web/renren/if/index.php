226c5aad874d5e4c6ff42a5e4d6e0035da01824e
lyb <big@playcrab.com>
2010-08-19 17:38:02 +0800

    jjd

--- web/renren/if/index.php
+++ web/renren/if/index.php
@@ -98,7 +98,7 @@ function install_swf(pid){
         <li class="freegift"><a href="../pop/gift.php" id="freeGift" >免费礼物</a></li>
         <li class="invite" ><a href="../pop/invite/invite.php" id="invite" >邀请好友</a></li>
         <li class="faq"><a id='faq'  href="../static/help/FAQ.html" >常见问题</a></li>
-        <li class="problem"><a href="javascript:alert('暂未开放');"  id="problem">问题反馈</a></li>
+        <li class="problem"><a href="javascript:sendNotifcation();" class="fullpage" id="problem">问题反馈</a></li>
 		<li class="payment" ><a  class='fullpage' href="http://apps.renren.com/livemall/pay.php"   target="_top" id ="pay">充值</a></li>
 	</ul>
 	</div>

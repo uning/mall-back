522abac7dd627cf2fc185da54ead115050c49211
unknown <Kevin@.(none)>
2010-08-19 10:20:02 +0800

    x

--- web/renren/pay.php
+++ web/renren/pay.php
@@ -117,12 +117,12 @@ height: 700px;
 				<div class="logo"><a href="http://apps.renren.com/livemall/" target="_top" title="开始游戏!">logo</a></div>
 			   <div id="tabs">
 				<ul class="clearfix tcenter">       
-					<li class="game" id="flashTab" ><a class="active" href="http://apps.renren.com/livemall" >游戏</a></li>
-					<li class="freegift"><a href="http://apps.renren.com/livemall#switchToGift" id="freeGift" >免费礼物</a></li>
-					<li class="invite" ><a href="http://apps.renren.com/livemall#switchToInvite" >邀请好友</a></li>
+					<li class="game" id="flashTab" ><a  href="http://apps.renren.com/livemall" >游戏</a></li>
+					<li class="freegift"><a href="http://apps.renren.com/livemall#freeGift" id="freeGift" >免费礼物</a></li>
+					<li class="invite" ><a href="http://apps.renren.com/livemall#invite" >邀请好友</a></li>
 					<li class="faq"><a  href="../static/help/teach.html" class="fullpage" target="_blank">常见问题</a></li>
 					<li class="problem"><a  href="javascript:alert('暂未开放');"  id="problem">问题反馈</a></li>
-					<li class="payment" ><a  href="http://apps.renren.com/livemall/pay.php"   id ="pay">充值</a></li>
+					<li class="payment" ><a  class="active" href="http://apps.renren.com/livemall/pay.php"   id ="pay">充值</a></li>
 				</ul>
 				</div>
 				</div>

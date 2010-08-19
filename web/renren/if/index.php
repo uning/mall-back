c276820a19c58c5f98f00c1d32cb6d7c0bda074f
unknown <Kevin@.(none)>
2010-08-19 11:06:46 +0800

    x

--- web/renren/if/index.php
+++ web/renren/if/index.php
@@ -29,7 +29,7 @@ if($gflg){
 <link rel="stylesheet"href="<?php echo RenrenConfig::$resource_urlp;?>css/main.css?2" />
 <link rel="shortcut icon" href="<?php echo RenrenConfig::$resource_urlp;?>images/favicon.ico" type="image/x-icon" />
 <script type="text/javascript">
-var a='<? php echo $_REQUEST['a']; ?>';
+var a='<?php echo $_REQUEST['a']; ?>';
 </script>
 <script src="<?php echo RenrenConfig::$resource_urlp;?>js/jquery-1.4.2.min.js"></script>
 <script src="<?php echo RenrenConfig::$resource_urlp;?>js/loader.js"></script>

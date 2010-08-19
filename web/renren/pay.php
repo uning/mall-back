f53a6fe9ca606cafc48863f5a9ded6b36cd05525
unknown <Kevin@.(none)>
2010-08-19 10:01:43 +0800

    x

--- web/renren/pay.php
+++ web/renren/pay.php
@@ -4,6 +4,9 @@ require_once('config.php');
 ?> 
 
  <style>  
+#content {
+height: 700px;
+}
 #header .logo {
     width: 165px;
     height: 46px;
@@ -115,8 +118,8 @@ require_once('config.php');
 			   <div id="tabs">
 				<ul class="clearfix tcenter">       
 					<li class="game" id="flashTab" ><a class="active" href="http://apps.renren.com/livemall" >游戏</a></li>
-					<li class="freegift"><a href="../pop/gift.php" id="freeGift" >免费礼物</a></li>
-					<li class="invite" ><a href="../pop/invite/invite.php" id="invite" >邀请好友</a></li>
+					<li class="freegift"><a href="http://apps.renren.com/livemall#switchToGift" id="freeGift" >免费礼物</a></li>
+					<li class="invite" ><a href="http://apps.renren.com/livemall#switchToInvite" >邀请好友</a></li>
 					<li class="faq"><a  href="../static/help/teach.html" class="fullpage" target="_blank">常见问题</a></li>
 					<li class="problem"><a  href="javascript:alert('暂未开放');"  id="problem">问题反馈</a></li>
 					<li class="payment" ><a  href="http://apps.renren.com/livemall/pay.php"   id ="pay">充值</a></li>

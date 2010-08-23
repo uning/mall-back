82914a0ff9135b93405905765a31cf23f7c51f55
lyb <big@playcrab.com>
2010-08-23 18:30:57 +0800

    jf

--- web/renren/pop/invite/invite.php
+++ web/renren/pop/invite/invite.php
@@ -62,7 +62,7 @@ if($_REQUEST['gift'])
 	$height = 750;
 ?>
 <body bgcolor="#ffffff">
-<div style="width:770px;height:<?php echo $height.'px';?>;border:#3399bb solid 1px;">
+<div style="width:760px;height:<?php echo $height.'px';?>;border:#3399bb solid 1px;">
 <table width="100%">
 <tr>
 <td align="right"><a  onclick="toFlash()" style="cursor: pointer;"><img src="../../static/images/css/close.png" border="0"/></a></td>
@@ -100,7 +100,7 @@ if($_REQUEST['gift'])
 	
 	$linkid = $pid.':'.uniqid();
 	
-	$width = '730px';
+	$width = '740px';
 	//print_r($exclude);
 	if($gid!=NULL&&$gid!=''){
 		 $accept_gift_url = RenrenConfig::$canvas_url."accept.php?linkid=$linkid";

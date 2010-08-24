d2efb0f549246ef62545589e1576d88e356670c8
lyb <big@playcrab.com>
2010-08-24 15:25:24 +0800

    jhd

--- web/renren/if/store_invite.php
+++ web/renren/if/store_invite.php
@@ -13,12 +13,22 @@ if($pid &&$ids && $linkid){
 		'geted'=> array(),
 		'ids' =>$_REQUEST['ids'],
 		'gift'=>$gid
-		));
+		),
+		'today'=>array('date'=>date('Ymd'),'invite'=>$_REQUEST['ids'],'getted'=>array())
+		);
 		$tw->put($value);
 		
 	}
 	else {
 		$value[$linkid]=array('geted'=>array(),'ids'=>$_REQUEST['ids'],'gift'=>$gid,'date'=>date('Ymd'));
+		if($value['today']['date']!=date('Ymd')){
+			$value['today'] = date('Ymd');
+			$value['today']['invite'] = $_REQUEST['ids'];
+			$value['today']['getted'] = array();
+		}
+		else{
+			array_merge($value['today']['invite'],$_REQUEST['ids'])	;
+		}
 		$tw->put($value);
 	}
 	print_r($value);

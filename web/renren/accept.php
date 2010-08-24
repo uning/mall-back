2fd58cea8590fa5846402dc5f08ae3cf277c9216
lyb <big@playcrab.com>
2010-08-24 12:25:19 +0800

    jhd

--- web/renren/accept.php
+++ web/renren/accept.php
@@ -1,10 +1,160 @@
+<style type="text/css">
+.padding_content {
+	padding: 8px;
+}
 
+.center {
+	text-align: center;
+}
+
+.gift_img {
+	padding: 17px 11px 0px 0px;
+	text-align: center;
+}
+
+.gift_name {
+	margin: 5px 17px 0px 5px;
+	color: #70461e;
+	font-weight: bold;
+}
+
+li.giftLocked .gift_name {
+	color: #666666;
+}
+
+.gift_name span {
+	display: block;
+	height: 30px;
+}
+
+.giftIconImg {
+	width: 75px;
+	height: 75px;
+}
+
+.main_gift_cont ul {
+	list-style-type: none;
+}
+
+.main_giftConfirm_cont {
+	background: #ffffff
+		url("http://rrmall.playcrab.com/work/mall/backend/web/renren/static/images/css/gift_confirm_box.png")
+		no-repeat scroll left top;
+	width: 744px;
+	height: 265px;
+}
+
+.main_giftConfirm_cont h3 {
+	color: #559890;
+	font-size: 18px;
+	padding-top: 15px;
+}
+
+.gift_box_holder {
+	padding: 34px 150px;
+}
+
+.gift_box_cont {
+	background: #ffffff
+		url("http://rrmall.playcrab.com/work/mall/backend/web/renren/static/images/css/gift_confirm_gift.png")
+		no-repeat scroll left top;
+	width: 152px;
+	height: 151px;
+	padding-top: 20px;
+	float: left;
+}
+
+.gift_from {
+	padding: 55px 0px 0px 62px;
+	float: left;
+}
+
+.gift_box_cont .giftConfirm_img {
+	height: 115px;
+}
+
+.gift_box_cont .giftConfirm_name {
+	color: #559890;
+	font-weight: bold;
+}
+
+.from_box_cont {
+	background: #ffffff
+		url("http://rrmall.playcrab.com/work/mall/backend/web/renren/static/images/css/gift_confirm_user.png")
+		no-repeat scroll left top;
+	width: 117px;
+	height: 115px;
+	padding-left: 1px;
+	padding-top: 23px;
+	float: right;
+	margin-top: 15px;
+}
+
+.giftFrom_img img {
+	height: 50px;
+}
+
+.giftFrom_name {
+	padding-top: 20px;
+}
+
+.giftFrom_name a:link,.giftFrom_name a:visited {
+	color: #3b5998;
+	font-weight: bold;
+	text-decoration: underline;
+}
+
+.giftFrom_name a:hover,.giftFrom_name a:active {
+	color: #559890;
+	text-decoration: none;
+}
+
+.giftformsubmit {
+	border-style: solid;
+	border-color: #d9dfea #0e1f5b #0e1f5b #d9dfea;
+	border-width: 1px;
+	margin: 1px 5px;
+	padding: 3px 10px;
+	background-color: #3b5998;
+	color: white;
+	font-size: 12px;
+	font-weight: bold;
+	text-decoration: none;
+	height: 25px;
+}
+
+.giftButtonFloat {
+	float: center;
+	margin-top: 1px;
+	margin-bottom: 12px;
+}
+.giftformsubmit {
+	border-style: solid;
+	border-color: #d9dfea #0e1f5b #0e1f5b #d9dfea;
+	border-width: 1px;
+	margin: 1px 5px;
+	padding: 3px 10px;
+	background-color: #3b5998;
+	color: white;
+	font-size: 12px;
+	font-weight: bold;
+	text-decoration: none;
+	height: 25px;
+}
+
+.giftButtonFloat {
+	float: center;
+	margin-top: 1px;
+	margin-bottom: 12px;
+}
+</style>
 <?php
 require_once('config.php');
 require_once('pop/freeGift.php');
 $linkid = $_REQUEST['linkid'];
+
 $tw = TT::LinkTT();
-list($pid,$str) = split(':',$linkid);
+list($pid,$str) = explode(':',$linkid);
 
 $irec = $tw->getbyuidx('uid',$pid);
 $fromuser = $pid;	
@@ -31,10 +181,30 @@ if(!$irec[$linkid]){?>
 	$tudata=$ftu->getf(array('name','icon'));
 	
 	$getted = $irec[$linkid]['geted'];
-	if(!$getted[$touser]){
+	$ids = $irec[$linkid]['ids'];
+	$invite = false;
+	foreach($ids as $id){
+		if($id==$touser){
+		$invite = true;
+		break;
+		}
+	}
+	?>
+	<?php if(!$invite){?>
+	<xn:redirect url="<?php echo RenrenConfig::$canvas_url;?>" />
+	<?php ?>
+	<?php
+	$got = false;
+	foreach ($getted as $u){
+		if($u==$touser){
+			$got = true;
+			break;
+		}
+	}
+	if(!$got&&$invite){
 		$ftu->numch('invite_num',1);
 	}
-	if(!$getted[$touser]){
+	if(!$got&&$invite){
 		$gid = $irec[$linkid]['gift'];
 		if($gid){?>
 	<div class="padding_content center" style="overflow: hidden;">
@@ -67,9 +237,8 @@ if(!$irec[$linkid]){?>
 			$data['pos']='s';
 			$ttu->puto( $data ); 
 		}
-		//$irec['invalid'] =  1;
-		$getted[$touser] = 1;
-		$irec[$linkid]['geted'] = $getted;
+		
+		$irec[$linkid]['geted'][] = $touser;
 		$tw->put($irec);
 		if(!$gid){?>
 		<xn:redirect url="<?php echo RenrenConfig::$canvas_url;?>" />
@@ -83,7 +252,9 @@ if(!$irec[$linkid]){?>
 		<xn:redirect
 			url="<?php 
 $next = RenrenConfig::$canvas_url."accept.php?linkid=$linkid"; 
-$rurl = 'http://app.renren.com/apps/tos.do?api_key='.RenrenConfig::$api_key.'&v=1.0&next='.urlencode($next);
+$tail = '&v=1.0&next='.urlencode($next);
+if(!$invite) $tail = '';
+$rurl = 'http://app.renren.com/apps/tos.do?api_key='.RenrenConfig::$api_key.$tail;
 echo $rurl;?>" />
 	</xn:else>
 </xn:if-is-app-user>

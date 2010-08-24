d4f58f7f453fc55b6d120509edc5d59bd40c7e88
lyb <big@playcrab.com>
2010-08-24 16:05:35 +0800

    jhd

--- web/renren/accept.php
+++ web/renren/accept.php
@@ -130,6 +130,7 @@ li.giftLocked .gift_name {
 	margin-top: 1px;
 	margin-bottom: 12px;
 }
+
 .giftformsubmit {
 	border-style: solid;
 	border-color: #d9dfea #0e1f5b #0e1f5b #d9dfea;
@@ -154,7 +155,6 @@ li.giftLocked .gift_name {
 require_once('config.php');
 require_once('pop/freeGift.php');
 $linkid = $_REQUEST['linkid'];
-$linkid = '45182749:4c734ae5c658b';
 $tw = TT::LinkTT();
 list($pid,$str) = explode(':',$linkid);
 
@@ -162,15 +162,15 @@ $irec = $tw->getbyuidx('uid',$pid);
 $link = &$irec[$linkid];
 $fromuser = $pid;	
 $touser = $_REQUEST['xn_sig_user'];	
-$touser =202150436;
 if(!$link){?>
 <xn:if-is-app-user>
-	<xn:redirect url="<?php echo RenrenConfig::$canvas_url.'?from=uinvite';?>" />
+	<xn:redirect
+		url="<?php echo RenrenConfig::$canvas_url.'?from=uinvite';?>" />
 	<xn:else>
 		<xn:redirect
 			url="<?php $rurl = 'http://app.renren.com/apps/tos.do?api_key='.RenrenConfig::$api_key.'&v=1.0&next='.RenrenConfig::$canvas_url;echo $rurl;?>" />
 	</xn:else>
-	</xn:if-is-app-user>
+</xn:if-is-app-user>
 <?php 
 			exit ;
 }
@@ -178,7 +178,7 @@ if(!$link){?>
 <xn:if-is-app-user>
 <?php
 	$fsess = TTGenid::getbypid($fromuser);	
-	$tsess = TTGenid::getbypid($touser);	
+	$tsess = TTGenid::genid($touser,true);	
 		$ftu = new TTUser($fsess['id']);
 		$ttu = new TTUser($tsess['id']);
 			$tudata=$ftu->getf(array('name','icon'));
@@ -186,47 +186,44 @@ if(!$link){?>
 				$ids = $link['ids'];
 				$invite = false;
 					foreach($ids as $id){
-								if($id==$touser){
-											$invite = true;
-													break;
-													}
-									}  
+						if($id==$touser){
+									$invite = true;
+									break;
+									}
+							}  
 					$got = false;
 					foreach ($getted as $u){
-								if($u==$touser){
-												$got = true;
-															break;
-														}
+							if($u==$touser){
+								$got = true;
+									break;
 									}
+							}
+						
 						if(!$got&&$invite){
-									$ftu->numch('invite_num',1);
-										}
-						if(!$got&&$invite){
-									$gid = $link['gift'];
-											if($gid){?>
+								$gid = $link['gift'];
+										if($gid){?>
 	<div class="padding_content center" style="overflow: hidden;">
 	<div class="main_giftConfirm_cont">
-		<h3>您接受了<?php echo $gift[$gid]['name'];?></h3>
+	<h3>您接受了<?php echo $gift[$gid]['name'];?></h3>
 	<div class="gift_box_holder">
-		<div class="gift_box_cont">
-			<div class="giftConfirm_img">
-			<img src="http://rrmall.playcrab.com/work/mall/backend/web/renren/static/images/giftIcon/<?php echo $gift[$gid]['icon'];?>"></img>
-			</div>
-			<div class="giftConfirm_name"><span><?php echo $gift[$gid]['name'];?></span></div>
-		</div>
-		<div class="gift_from">
-			<h3>From</h3>
-		</div>
-		<div class="from_box_cont">
-			<div class="giftFrom_img"><img src="<?php echo $tudata['icon'];?>" /></div>
-			<div class="giftFrom_name"><span><?php echo $tudata['name'];?></span></div>
-		</div>
+	<div class="gift_box_cont">
+	<div class="giftConfirm_img"><img
+		src="http://rrmall.playcrab.com/work/mall/backend/web/renren/static/images/giftIcon/<?php echo $gift[$gid]['icon'];?>"></img>
+	</div>
+	<div class="giftConfirm_name"><span><?php echo $gift[$gid]['name'];?></span></div>
+	</div>
+	<div class="gift_from">
+	<h3>From</h3>
+	</div>
+	<div class="from_box_cont">
+	<div class="giftFrom_img"><img src="<?php echo $tudata['icon'];?>" /></div>
+	<div class="giftFrom_name"><span><?php echo $tudata['name'];?></span></div>
+	</div>
 	</div>
 	</div>
 	</div>
 	<div align="center"><a href="http://apps.renren.com/livemall"
-		class="giftformsubmit giftButtonFloat">~游戏去~</a>
-	</div>
+		class="giftformsubmit giftButtonFloat">~游戏去~</a></div>
 		<?php 
 			$id = $ttu->getdid( '',$gift[$gid]['group'] );
 			$data['tag'] = $gid;
@@ -235,17 +232,18 @@ if(!$link){?>
 			$ttu->puto( $data ); 
 		}
 		
-											$link['geted'][] = $touser;
-											$tw->put($irec);
-													if(!$gid){?>
+		$link['geted'][] = $touser;
+		$tw->put($irec);
+		if(!$gid){?>
 		<xn:redirect url="<?php echo RenrenConfig::$canvas_url;?>" />
 		<?php }
 		}
 		else{?>
 			<div align="center"><a href="http://apps.renren.com/livemall"
-				class="giftformsubmit giftButtonFloat">~游戏去~</a></div>
+		class="giftformsubmit giftButtonFloat">~游戏去~</a></div>
 		<?php }?>
 	<xn:else>
+
 		<xn:redirect
 			url="<?php 
 			$next = RenrenConfig::$canvas_url."accept.php?linkid=$linkid"; 

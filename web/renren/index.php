54624ecd606ea5ed80ac781e72e32e7c920aba0b
Kevin <kevin@playcrab.com>
2010-08-30 14:41:46 +0800

    x

--- web/renren/index.php
+++ web/renren/index.php
@@ -13,6 +13,12 @@ border:1px solid #BEB9B9;
 margin:5px;
 padding:5px;
 }
+.bottom {
+width:760px; 
+}
+.bottom a img{
+border: 0;
+}
 </style>
 <div id='is_install'></div>
 <div id='content'>
@@ -20,6 +26,11 @@ padding:5px;
         <div class='canvas'>
 	        <xn:iframe scrolling="no" src="<?php echo RenrenConfig::$callback_url?>if/index.php?a=<?php echo $_REQUEST['a']; ?>" height="770" width="800" frameborder="0"/>
 		</div> 
+		<div class='bottom'>
+			<a target='_blank' href='http://page.renren.com/livemall'>
+				<img src="<?php echo RenrenConfig::$resource_urlp ?>images/bottom.png"></img>
+			</a>
+		</div>
 		<div class='footnotice'>
 			健康忠告：抵制不良游戏，拒绝盗版游戏。注意自我保护，预防受骗上当。适度游戏益脑，沉迷游戏伤身。合理安排时间，享受健康生活。				    				
 		</div>

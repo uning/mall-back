fa0e98e55754dd5c845ec35dccf7c556a9ea6011
Kevin <kevin@playcrab.com>
2010-09-04 15:18:15 +0800

    x

--- web/public/tools/add_user_money.php
+++ web/public/tools/add_user_money.php
@@ -4,45 +4,43 @@
 <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
 <META HTTP-EQUIV="Expires" CONTENT="-1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>活力商城</title>
+  <title>加金币和宝石</title>
 <body>
 <?php
-echo "<pre>\n<form method='get'>";
 require_once '../base.php';
-$u=$argv[1];
+$u=$_REQUEST['u'];
 if(!$u){
-	$u=$_REQUEST['u'];
-}
-if(!$u){
- 	$pid=$_REQUEST['pid']; 
-	if($pid)
-		$data = TTGenid::getbypid($pid);
+ 	$pid=$_REQUEST['pid'];  
+    if($pid)
+	  $data = TTGenid::getbypid($pid);
 }else
 	$data = TTGenid::getbyid($u);
 
 if(!$data||$data['id']){
-	echo "<p>PID: <input name='pid' type='text' /></p>";
-}
-	echo "<p>金币: <input name='money' value='5000' type='text' /></p>";
-	echo "<p>宝石: <input name='gem' value='0' type='text' /></p>";
-	
-
-
-if($_REQUEST['money'] && $_REQUEST['gem'] &&$_REQUEST['money']>=0 &&$_REQUEST['gem']>=0&&$_REQUEST['money']<=1000000000 &&$_REQUEST['gem']<=1000){
-	$u = $data['id'];
+	echo "<p><form method='get'>PID: <input name='pid' type='text' /><input type='submit' value='获取用户' /> </form></p>";
+}else{ 
+    $u = $data['id'];
 	if(!$u)
 		die( "no u get");
+    
 	$tu = new TTUser($u); 
-	$tu->numch('money',$_REQUEST['money']);
-	$tu->numch('gem',$_REQUEST['gem']);
-	echo "<p>OK</p>";
-
-}else{
-	echo "<p>输入不合法</p>";
+    echo "<p><a href='add_user_money.php'>换个用户</a>&nbsp&nbsp;&nbsp;<a href='get_user.php?u=".$u."'>用户信息</a><p>";
+    echo "<p>现有金币".$tu->chMoney(0).",宝石".$tu->chGem(0)."</p><br/><br/>";
+	
+	echo "<form method='post'>";
+	echo "<p>金币: <input name='money' value='5000' type='text' /></p>";
+	echo "<p>宝石: <input ame='gem' value='0' type='text' /></p>";
+	 
+	if($_POST['money'] && $_POST['gem'] &&$_POST['money']>=0 &&$_POST['gem']>=0&&$_POST['money']<=1000000000 &&$_POST['gem']<=1000){
+		
+		$tu->numch('money',$_POST['money']);
+		$tu->numch('gem',$_POST['gem']);
+		echo "<p>OK".</p>"; 
+	}else{
+		echo "<p>输入不合法</p>";
+	} 
+    echo "<p><input type='submit' value='确定'/></p></form>";
 }
-
-echo "<p><input type='submit' value='确定'/></p></form>";
-
 ?>
 </body>
 </html>

d45af52c252bea3ec6fcb18d0ab338fadcc5cc3a
lyb <big@playcrab.com>
2010-09-07 16:09:02 +0800

    jkdd

--- web/renren/test.php
+++ web/renren/test.php
@@ -4,14 +4,13 @@ function test(ok){
 }
 
 var feedSettings
- = {"template_id":"1",
-"template_data":{"images":
+ = {"template_bundle_id":"1",
+	"template_data":{"images":
   [{"src":"http://fmn042.xnimg.cn/fmn042/20090806/0905/p_large_oQSJ_2633m016062.jpg",
   "href":"http://dev.renren.com/developers/portal.do"}],
-  "title":"ttt",
   "content":"支持校内，情系人人！",
   "action":"这里是的值",
-  "rruid":"这里是的值"
+  "xnuid":"这里是的值"
 },
 "body_general":"这里用来显示 body_general",
 "callback": test,

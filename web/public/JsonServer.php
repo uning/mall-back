8da8d1faed1d7c8932a14d35131c100b8235918e
Jimmy <zxm@playcrab.com>
2010-09-07 12:10:31 +0800

    delete man.satisfy stat

--- web/public/JsonServer.php
+++ web/public/JsonServer.php
@@ -189,7 +189,6 @@ class JsonServer{
 				'Advert.set'=>1,
 				'Cinema.enter'=>1,
 				'Cinema.pick'=>1,
-		        'Man.satisfy'=>1,
 		        'UserController.update_profile'=>1,
 				'ItemController.buy'=>1,
 				'ItemController.sale'=>1,
